bc25ef7a67e94303da1e821f93756a52d12b9ac0
loljoho <joho@joho.io>
2017-05-27 16:07:11 -0400

    Tidy up app.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+
+//  AngularJS Modules
+// ------------------------------
+
 import angular from 'angular';
 import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
@@ -7,23 +11,35 @@ import ngCookies from 'angular-cookies';
 import ngResource from 'angular-resource';
 import ngSanitize from 'angular-sanitize';
 import ngTouch from 'angular-touch';
+
+
+//  3rd Party Vendor Modules
+// ------------------------------
+
 import angularLoadingBar from 'angular-loading-bar';
 import uiRouter from 'angular-ui-router';
 import uiBootstrap from 'angular-ui-bootstrap';
 
-import {
-  routeConfig
-} from './app.config';
 
-import {
-  runBlock
-} from './app.run';
+//  Config/Run
+// ------------------------------
+
+import { routeConfig } from './app.config';
+import { runBlock } from './app.run';
+
+
+//  Components
+// ------------------------------
 
 import navbar from '../components/navbar/navbar.component';
 import footer from '../components/footer/footer.component';
 import main from './main/main.component';
 import constants from './app.constants';
 import util from '../components/util/util.module';
+
+
+//  Styles
+// ------------------------------
 
 import './app.scss';
 
@@ -48,6 +64,10 @@ angular.module('gpqueryApp', [
 ])
   .config(routeConfig)
   .run(runBlock);
+
+
+//  Bootstrap Application
+// ------------------------------
 
 angular.element(document)
   .ready(() => {
